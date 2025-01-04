08cedd2d69d573e970d8ae5ea8b052f65018d870
vijay <vijay15julychaurasiya@gmail.com>
2025-01-04 09:13:17 -0800

    I updated some line of code

--- Java Project/B.java
+++ Java Project/B.java
@@ -1,7 +1,18 @@
 class B{
+
     public static void main(String[] args) {
         for(int i=0;i<=10;i++){
             System.out.print("I Love you");
         }
+
+        for(int i=0;i<=10;i++){
+            System.out.print("I Love you");
+        }
+        
+        for(int i=0;i<=10;i++){
+            System.out.print("I Love you");
+        }
+        
     }
+
 }
